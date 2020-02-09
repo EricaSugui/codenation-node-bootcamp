59b5130b79ee7f9d82fe32c6b7c14f9837112dc7
Erica Akira <ericasugui@gmail.com>
2020-02-09 20:27:40 -0300

    corrigindo conflitos

--- modulo-3/exercicios/fileSystem.js
+++ modulo-3/exercicios/fileSystem.js
@@ -1,2 +1,50 @@
 const fs = require('fs')
 const { toHTML } = require('./template')
+const csv = require('csvtojson')
+
+
+function loadCSV(filename) {
+    const path = `${process.cwd()}/modulo-3/exercicios/assets/${filename}.csv`
+
+    return new Promise((resolve, reject) => {
+        
+        fs.readFile(path, 'utf-8', (error, data) => {
+            if(error) reject(error)
+
+            resolve(data)
+        })
+    })
+}
+
+async function parseCSV(csvStr) {
+    return await csv()
+    .fromString(csvStr)
+}
+
+async function createHTML(movies, filename) {
+    const path = `${process.cwd()}/modulo-3/exercicios/assets/${filename}.csv`
+
+    Promise.all(movies.map(movie => {
+        return new Promise((resolve, reject) =>{
+            const html = toHTML(movie)
+
+            fs.writeFile(path, (error, result) => {
+                if(error) {
+                    reject(error)
+                }
+
+                resolve('Create with success')
+            })
+         })
+        
+    }))
+    //return new Promise((resolve, reject) => {
+    //    fs.writeFile(path,)
+    //})
+}
+
+module.exports = {
+    loadCSV,
+    parseCSV,
+    createHTML
+}
